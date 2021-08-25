1891f5bd6cc41d76dd6d23c0a75da1d4b111c6dc
master <enne3939@gmail.com>
2021-08-25 15:33:50 +0900

    끝말잇기 class만들기 && 웹팩 데브 서버와 핫리로딩

--- lecture/webpack.config.js
+++ lecture/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
 	name: 'word-relay-dev',
 	mode: 'development', //실서비스: production
 	devtool: 'inline-source-map',
 	resolve: {
-		extensions: ['js', '.jsx'],
-	},//입력 확장자 생략 가능하도록 감지해주는 곳.
+    extensions: ['.js', '.jsx'],
+  },//입력 확장자 생략 가능하도록 감지해주는 곳.
 
 	entry: {
 		app: ['./client'],
@@ -17,15 +18,36 @@ module.exports = {
 			test: /\.jsx?/,
 			loader: 'babel-loader',
 			options: {
-				presets: ['@babel/preset-env','@babel/preset-react'],
-				plugins: ['@babel/plugin-proposal-class-properties'],
+				presets: [
+					['@babel/preset-env',{
+						targets: {
+							browsers: ['> 1% in KR'],
+						},
+						debug: true,
+					}],
+					'@babel/preset-react'
+				],
+				plugins: [
+					'@babel/plugin-proposal-class-properties',
+					'react-refresh/babel',
+				],
 			},
       exclude: path.join(__dirname, 'node_modules'),
 		}],
 	},
 
+	plugins: [
+		new RefreshWebpackPlugin()
+	],
+
 	output: {
     path: path.join(__dirname, 'dist'),
-		filename: 'app.js'
+		filename: 'app.js',
+		publicPath: '/dist/',
 	},//출력
+
+	devServer: {
+		publicPath: '/dist/', 
+		hot: true,
+	},
 };
