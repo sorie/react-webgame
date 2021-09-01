7b37fff9518cbbc69261bf7d910fb31d84ee41d7
master <enne3939@gmail.com>
2021-09-01 17:09:03 +0900

    반응속도체크

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import NumberBaseball from './NumberBaseball';
+import RSP from './RSP';
 
-ReactDOM.render(<NumberBaseball />, document.querySelector('#root'));
+ReactDOM.render(<RSP />, document.querySelector('#root'));
